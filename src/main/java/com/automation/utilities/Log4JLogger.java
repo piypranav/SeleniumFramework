c0db5fb782fc5995679d7ab1b156be7251cc65f2
piypranav <piy.pranav@gmail.com>
2020-05-20 23:25:46 +0530

    Log4j2 properties code checkin

---
+++ src/main/java/com/automation/utilities/Log4JLogger.java
@@ -0,0 +1,9 @@
+package com.automation.utilities;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+public class Log4JLogger {
+
+    Logger logger = LogManager.getLogger()
+}
